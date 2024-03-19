15c579c852f6aa286a189a26b87455c1000430d3
Gašper Novak <gasper.novak@inova.si>
2024-03-19 15:10:06 +0100

    Added timer and wpm counter

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ function App() {
   return (
     <>
       <Typetest/>
-      <Button $primary>test</Button>
     </>
   )
 }
--- src/components/Typetest.tsx
+++ src/components/Typetest.tsx
@@ -2,11 +2,12 @@ import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 import { doc, getDoc } from "firebase/firestore";
 import { db } from '../firebase';
+import { useTimer } from 'react-timer-hook';
 
 const Wrapper = styled.section`
-    font-size: 1.5em;
+    font-size: 1.2em;
     text-align: left;
-    padding: 10px;
+    padding: 10px 10px;
     border-radius: 4px;
     border: 1px solid black;
     color: antiquewhite;
@@ -19,6 +20,7 @@ const Words = styled.div`
     padding: 0.5em;
     margin-bottom: 10px;
     color: white;
+    text-align: justify;
 `
 
 const Input = styled.input`
@@ -30,13 +32,42 @@ const Input = styled.input`
     outline: none;
 `
 
+const Info = styled.div`
+    display: flex;
+    justify-content: space-between;
+`
+
+const InfoItem = styled.span`
+    margin-bottom: 10px;
+`
+
+const calculateWpm = (allEntries: number, timeSeconds: number) => {
+    return Math.floor((allEntries/5) / (timeSeconds/60))
+}
+
 const Typetest = () => {
     const [wordlist, setWordlist] = useState<String[]>([])
+    const [wpm, setWpm] = useState(0);
+    const [entriesCounter, setEntriesCounter] = useState(0);
+    const [timeOver, setTimeOver] = useState(false);
+    
+    const timeLimitSeconds = 15;
+    const time = new Date();
+    time.setSeconds(time.getSeconds()+timeLimitSeconds)
+
+    const timer = useTimer({expiryTimestamp: time,
+        onExpire: () => {
+            setTimeOver(true);
+        },
+        autoStart: false
+    });
 
     const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+        timer.start()
         if(e.currentTarget.value === wordlist[0]+' '){
             setWordlist(wordlist.slice(1))
             e.currentTarget.value = ''
+            setEntriesCounter(entriesCounter+wordlist[0].length+1)
         }
     }
 
@@ -56,9 +87,20 @@ const Typetest = () => {
         fetchWordlist()
     },[])
 
+    useEffect(() => {
+        console.log(entriesCounter, timeLimitSeconds-timer.seconds)
+        if(timer.isRunning){
+            setWpm(calculateWpm(entriesCounter, (timeLimitSeconds-timer.seconds)))
+        }
+    }, [timer.seconds])
+
     return (
     <div>
         <Wrapper>
+            <Info>
+                <InfoItem>WPM: {wpm}</InfoItem>
+                <InfoItem>{timer.minutes}:{timer.seconds}</InfoItem>
+            </Info>
             <Words>
                 { wordlist &&
                     wordlist.map((word, key) => {
@@ -66,7 +108,7 @@ const Typetest = () => {
                     })
                 }
             </Words>
-            <Input onChange={handleInput} />
+            <Input onChange={handleInput} disabled={timeOver}/>
         </Wrapper>
     </div>
   )
